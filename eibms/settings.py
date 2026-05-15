da950590d77ce991d92970d7a8673bff1774e17c
Mchomvuakida <mchomvuakida242@gmail.com>
2026-05-15 12:13:08 +0200

    Add logging to debug production 500 error

--- eibms/settings.py
+++ eibms/settings.py
@@ -152,3 +152,23 @@ REST_FRAMEWORK = {
     ),
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
